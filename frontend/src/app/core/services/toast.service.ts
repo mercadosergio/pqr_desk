fc282a4fee2250228b1217098b4fa2c74eb248ec
Sergio Mercado <mercadosergio754@gmail.com>
2026-09-18 14:32:26 -0500

    layout changes

--- frontend/src/app/core/services/toast.service.ts
+++ frontend/src/app/core/services/toast.service.ts
@@ -5,7 +5,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class ToastService {
   private snackBar = inject(MatSnackBar);
 
-  show(message: string, type: 'success' | 'error' | 'warning' = 'success', duration = 20000) {
+  show(message: string, type: 'success' | 'error' | 'warning' = 'success', duration = 3000) {
     this.snackBar.open(message, 'Cerrar', {
       duration,
       horizontalPosition: 'left',
