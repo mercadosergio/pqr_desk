786b4356ec4f67f2c6744079608e6b5a234f9f83
Sergio Mercado <mercadosergio754@gmail.com>
2026-09-18 13:46:51 -0500

    dockerizar API REST

--- api/pqrapi/settings.py
+++ api/pqrapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,11 +106,12 @@ WSGI_APPLICATION = "pqrapi.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.environ.get("SQLITE_PATH", BASE_DIR / "db.sqlite3"),
     }
 }
 
